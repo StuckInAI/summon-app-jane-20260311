fc7673af73a1e0e56b250c152e2176e4b7033f2f
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-11 21:30:42 +0500

    Add src/lib/database.ts

---
+++ src/lib/database.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Todo } from '@/entities/Todo';
+import path from 'path';
+
+const dbPath = process.env.DATABASE_PATH
+  ? path.resolve(process.env.DATABASE_PATH)
+  : path.resolve('./database.sqlite');
+
+let dataSource: DataSource | null = null;
+
+export async function getDataSource(): Promise<DataSource> {
+  if (dataSource && dataSource.isInitialized) {
+    return dataSource;
+  }
+
+  dataSource = new DataSource({
+    type: 'better-sqlite3',
+    database: dbPath,
+    entities: [Todo],
+    synchronize: true,
+    logging: false,
+  });
+
+  await dataSource.initialize();
+  return dataSource;
+}
+
+export async function getTodoRepository() {
+  const ds = await getDataSource();
+  return ds.getRepository(Todo);
+}
